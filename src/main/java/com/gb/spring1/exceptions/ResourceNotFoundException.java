0a57f64c7b02aec65ad81c138ddba2131c7e2763
DeN <den.denis.denis@mail.ru>
2023-02-09 22:14:53 +0300

    Spring2 Lesson6

--- src/main/java/com/gb/spring1/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.gb.spring1.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
